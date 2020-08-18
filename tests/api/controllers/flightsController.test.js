45a59fb8fed83befc2c9a250c73023e803c2d4df
Dmitriy Purtseladze <purtss@gmail.com>
2020-08-18 17:43:07 +0200

    flightsController tests

---
+++ tests/api/controllers/flightsController.test.js
@@ -0,0 +1,51 @@
+const GetFlightsUseCase = require('../../../app/application/useCases/getFlightsUseCase');
+const FlightsController = require('../../../app/api/controllers/flightsController');
+const ServiceLocator = require('../../../app/infrastructure/config/service-locator');
+
+jest.mock('../../../app/application/useCases/getFlightsUseCase');
+jest.mock('../../../app/infrastructure/config/service-locator');
+
+describe('#flightsController', () => {
+
+    test('should return flights', async () => {
+        // arrange
+        const flights = [
+            {
+                "slices": [
+                    {
+                        "origin_name": "Schonefeld",
+                        "destination_name": "Stansted",
+                        "departure_date_time_utc": "2019-08-08T04:30:00.000Z",
+                        "arrival_date_time_utc": "2019-08-08T06:25:00.000Z",
+                        "flight_number": "144",
+                        "duration": 115
+                    },
+                    {
+                        "origin_name": "Stansted",
+                        "destination_name": "Schonefeld",
+                        "departure_date_time_utc": "2019-08-10T05:35:00.000Z",
+                        "arrival_date_time_utc": "2019-08-10T07:35:00.000Z",
+                        "flight_number": "8542",
+                        "duration": 120
+                    }
+                ],
+                "price": 129
+            }
+        ];
+        GetFlightsUseCase.mockImplementationOnce(() => flights);
+        
+        const request = {
+            server: {
+                app: {
+                    ServiceLocator
+                }
+            }
+        }
+
+        // act
+        const response = await FlightsController.getFlights(request);
+
+        // assert
+        expect(response).toBe(flights);
+    });
+});
